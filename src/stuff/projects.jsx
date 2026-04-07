d1dbb29282c6726cf9ebc4887cb1c2a0ba75ce2d
Lavnish <lavjhn@gmail.com>
2026-04-07 20:53:36 +0530

    Add Projects section

---
+++ src/stuff/projects.jsx
@@ -0,0 +1,25 @@
+import { PROJECTS } from "./data";
+
+export default function Projects() {
+
+    return (
+        <section className="section">
+            <h2 className="section-title">My Work:</h2>
+
+            <div>
+                {PROJECTS.map(p => (
+                    <div
+                        key={p.id}
+                        className="project-card"
+                    >
+                        <div style={{ height: 100 }} />
+                        <div>
+                            <h4>{p.name}</h4>
+                            <p>{p.desc}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </section>
+    );
+}
